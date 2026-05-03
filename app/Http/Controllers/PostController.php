e7bbd98b4753bd828ab9290bf5d44ffc9262a6d2
benzzco <benaziccodaffa54321@gmail.com>
2026-05-03 15:01:53 +0700

    RC-13: I do create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth; 
 class PostController extends Controller {
     
-
     public function index() {
         $posts = Post::latest()->get();
         return view('community.index', compact('posts'));
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $imagePath = null;
+        
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('community_images', 'public');
+        }
+
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'image_path' => $imagePath,
+            'users_id' => Auth::id(), 
+            'upvote_count' => 0,
+        ]);
+
+        return redirect()->route('community.index');
     }
 }
