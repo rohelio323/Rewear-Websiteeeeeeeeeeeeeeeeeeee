b2502dc78270a4be583e9e4c134925c5f3441eba
rohelio323 <roheliomarusis@gmail.com>
2026-06-14 16:50:55 +0700

    RC-40: Review List Management - seller can view all received reviews
    Implemented seller review list management. Sellers can now view all received reviews at /my-reviews, and their profile is publicly accessible at /seller/{id} by clicking their name on the product page. This completes PBI-38, PBI-39, and PBI-40.

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,16 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// [PBI-01] Admin Routes to define and manage CO2 constants
 Route::post('/admin/categories', [CO2Controller::class, 'addCategory']);
 Route::put('/admin/categories/{id}/co2-constant', [CO2Controller::class, 'updateCategoryCO2']);
 Route::delete('/admin/categories/{id}', [CO2Controller::class, 'deleteCategory']);
 
-// Marketplace
 Route::get('/marketplace', [ItemController::class, 'index'])->name('marketplace.index');
 Route::get('/item/detail/{item}', [ItemController::class, 'show'])->name('items.show');
 Route::get('/community', [PostController::class, 'index'])->name('community.index');
 
-// Public Seller Profile
 Route::get('/seller/{user}', [ReviewController::class, 'sellerProfile'])->name('seller.profile');
 
-// Challenges
 Route::get('/challenges', [App\Http\Controllers\ChallengeController::class, 'index'])->name('challenges.index');
 Route::get('/challenges/{challenge}', [App\Http\Controllers\ChallengeController::class, 'show'])->name('challenges.show');
 
@@ -42,7 +38,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/my-stats', [App\Http\Controllers\ProfileStatsController::class, 'index'])->name('profile.stats');
     Route::post('/profile/seller-apply', [ProfileController::class, 'applyAsSeller'])->name('seller.apply.submit');
 
-    // Orders
     Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
     Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
     Route::get('/orders/{order}/payment', [OrderController::class, 'paymentForm'])->name('orders.payment');
@@ -63,17 +58,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/favorites', [WishlistController::class, 'index'])->name('favorites.index');
     Route::post('/favorites/{item}/toggle', [WishlistController::class, 'toggle'])->name('favorites.toggle');
 
-    // Reviews
     Route::get('/orders/{order}/review', [ReviewController::class, 'create'])->name('reviews.create');
     Route::post('/orders/{order}/review', [ReviewController::class, 'store'])->name('reviews.store');
-
-    // Seller review list
     Route::get('/my-reviews', [ReviewController::class, 'index'])->name('reviews.index');
 
     Route::post('/reports', [ReportsController::class, 'store'])->name('reports.store');
 });
 
-// Seller Middleware
 Route::middleware(['auth', 'seller'])->group(function () {
     Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
     Route::post('/items', [ItemController::class, 'store'])->name('items.store');
@@ -82,7 +73,6 @@ Route::middleware(['auth', 'seller'])->group(function () {
     Route::delete('/items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
 });
 
-// Admin
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     Route::get('/users', [AdminUserController::class, 'index'])->name('users.index');
@@ -94,10 +84,10 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
     Route::post('/seller-requests/{user}/reject', [AdminUserController::class, 'rejectSeller'])->name('seller-requests.reject');
     Route::get('/moderation', [\App\Http\Controllers\Admin\AdminModerationController::class, 'index'])->name('moderation.index');
     Route::get('/moderation/{report}', [\App\Http\Controllers\Admin\AdminModerationController::class, 'show'])->name('moderation.show');
-    Route::post('/moderation/{report}/hide',    [\App\Http\Controllers\Admin\AdminModerationController::class, 'hide'])->name('moderation.hide');
-    Route::post('/moderation/{report}/delete',  [\App\Http\Controllers\Admin\AdminModerationController::class, 'delete'])->name('moderation.delete');
+    Route::post('/moderation/{report}/hide', [\App\Http\Controllers\Admin\AdminModerationController::class, 'hide'])->name('moderation.hide');
+    Route::post('/moderation/{report}/delete', [\App\Http\Controllers\Admin\AdminModerationController::class, 'delete'])->name('moderation.delete');
     Route::post('/moderation/{report}/dismiss', [\App\Http\Controllers\Admin\AdminModerationController::class, 'dismiss'])->name('moderation.dismiss');
-    Route::post('/moderation/{report}/warn',    [\App\Http\Controllers\Admin\AdminModerationController::class, 'warn'])->name('moderation.warn');
+    Route::post('/moderation/{report}/warn', [\App\Http\Controllers\Admin\AdminModerationController::class, 'warn'])->name('moderation.warn');
     Route::get('/challenges', [App\Http\Controllers\Admin\AdminChallengeController::class, 'index'])->name('challenges.index');
     Route::post('/challenges', [App\Http\Controllers\Admin\AdminChallengeController::class, 'store'])->name('challenges.store');
     Route::get('/challenges/{id}/edit', [App\Http\Controllers\Admin\AdminChallengeController::class, 'edit'])->name('challenges.edit');
